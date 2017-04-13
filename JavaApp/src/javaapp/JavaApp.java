25b62a8c6df03bb26685e48489dfd3c78a24ede0
69752150225 <69752150225@JIPA-LABIN05-24.ifro.local>
2017-04-12 21:38:49 -0400

    acrentado valores para fazer calculos desejados

--- JavaApp/src/javaapp/JavaApp.java
+++ JavaApp/src/javaapp/JavaApp.java
@@ -6,7 +6,11 @@ public class JavaApp {
 
    
     public static void main(String[] args) {
+        int x;
+        double y;
+            System.out.println(" digite a pimeira nota : ");
         
+            
     }
     
 }
